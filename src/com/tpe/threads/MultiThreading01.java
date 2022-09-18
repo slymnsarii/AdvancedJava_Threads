071315fb7c8b9b0b65c24fa5ddecc7d204376029
slymnsarii <slymnsarii@gmail.com>
2022-09-18 17:28:41 +0300

    18/09/2022

--- src/com/tpe/threads/MultiThreading01.java
+++ src/com/tpe/threads/MultiThreading01.java
@@ -5,10 +5,10 @@ public class MultiThreading01 {
 	public static void main(String[] args) throws InterruptedException {
 
 		long startTime= System.currentTimeMillis();
-		CounterWithoutMultiThread counter1=new CounterWithoutMultiThread(1);
+		CounterWithoutMultiThread counter1=new CounterWithoutMultiThread(1); //2
 		CounterWithoutMultiThread counter2=new CounterWithoutMultiThread(2);
 		
-		counter1.countMe();
+		counter1.countMe(); //bunu calistitip 5 sn sonra digerine geciyor
 		
 		System.out.println("----------------------------");
 		
@@ -21,21 +21,21 @@ public class MultiThreading01 {
 		
 		System.out.println("**************************************************");
 		
-		long startTime2= System.currentTimeMillis();
+		long startTime2= System.currentTimeMillis(); //4
 		CounterWithMultiThread counter3=new CounterWithMultiThread(1);
 		CounterWithMultiThread counter4=new CounterWithMultiThread(2);
 		
-		counter3.start();
+		counter3.start(); //stat'ı calistirinca run methodu calisiyor 
 		
 		System.out.println("----------------------------");
 		
-		counter4.start();
+		counter4.start();//ama calismanin bbitmesini beklemeden bu calismaya basliyor
 		
 		//Thread.sleep(6000);
 		
 		//join--Waits for this thread to die. 
-		counter3.join();
-		counter4.join();
+		counter3.join(); //bunlari ekleme sebebim counter3 threadi bitene kadar bunu beklet
+		counter4.join();//endTime'in dogru cikmasi icin
 		
 		long endTime2=System.currentTimeMillis();
 		
@@ -48,7 +48,7 @@ public class MultiThreading01 {
 
 }
 
-class CounterWithoutMultiThread{
+class CounterWithoutMultiThread{ //1
 	private int threadNum;
 	
 	public CounterWithoutMultiThread(int threadNum) {
@@ -57,7 +57,7 @@ class CounterWithoutMultiThread{
 	
 	public void countMe() throws InterruptedException {
 		for (int i = 1; i <=10; i++) {
-			Thread.sleep(500);
+			Thread.sleep(500); //main thread'i durduruyor burda
 			
 			System.out.println("i"+i+"Thread number:"+threadNum);
 		}
@@ -78,7 +78,7 @@ class CounterWithMultiThread extends Thread{
 		}
 	}
 	
-	public CounterWithMultiThread(int threadNum) {
+	public CounterWithMultiThread(int threadNum) {//3
 		this.threadNum=threadNum;
 	}
 	
